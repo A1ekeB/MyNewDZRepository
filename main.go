98ae6ba756fb1fccbd3af40cca16de16946137e1
Baskanbayev Alnur <baskanbaev.alnur@gmail.com>
2024-09-07 02:12:37 +0500

    My GIT homework second

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	//	var a bool = true    //Boolean (true/false)
+	//	var b int = 5        //Integer
+	//	var c float32 = 3.14 //Floating point number
+	//	var d string = "Hi!" //String
+	//	fmt.Println("Boolean: ", a)
+	//	fmt.Println("Integer: ", b)
+	//	fmt.Println("Float: ", c)
+	//	fmt.Println("String: ", d)
+	// +
+	// -dd
+	// *
+	// /
+	// %
+	a := 10
+	b := 31
+	result := b % a
+	// izmenil fail
+	//treeti izmena
+	fmt.Println(result)
+
+}
